b12c368578889843c850d372c1d40a711ca16f62
Faysal chowdhury <faysalsookh@gmail.com>
2023-10-23 14:27:39 +0600

    working on leave summary and leave request type UI

--- lib/page/leave/view/leave_type/leave_request_type.dart
+++ lib/page/leave/view/leave_type/leave_request_type.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:onesthrm/page/leave/view/leave_calender/leave_calender.dart';
 import 'package:onesthrm/res/nav_utail.dart';
+import '../../../../res/widgets/custom_button.dart';
 
 class LeaveRequestType extends StatelessWidget {
   const LeaveRequestType({Key? key}) : super(key: key);
@@ -12,16 +13,17 @@ class LeaveRequestType extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Leave Request Type"),
         ),
-        body: ListView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            shrinkWrap: true,
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {
-                  NavUtil.navigateScreen(context, const LeaveCalender());
-                },
-                child: Card(
+        body: Column(
+          children: [
+            ListView.separated(
+              padding: const EdgeInsets.all(12),
+              shrinkWrap: true,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
                   elevation: 4,
                   child: RadioListTile<int?>(
                     value: 0,
@@ -45,8 +47,22 @@ class LeaveRequestType extends StatelessWidget {
                     groupValue: 0,
                     onChanged: (int? value) {},
                   ),
-                ),
-              );
-            }));
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const SizedBox(
+                height: 5,
+              ),
+            ),
+             CustomButton(
+              title: "Next",
+              padding: 16,
+               clickButton: (){
+                NavUtil.navigateScreen(context, const LeaveCalender());
+               },
+
+            )
+          ],
+        ));
   }
 }
