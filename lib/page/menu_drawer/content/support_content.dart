bffbf9f8577b6308afffb19c2b9c6c6cf22f2e14
impexdhaka <mh72156@gmail.com>
2023-11-07 15:28:50 +0600

    language screen navigation fix

--- lib/page/menu_drawer/content/support_content.dart
+++ lib/page/menu_drawer/content/support_content.dart
@@ -6,6 +6,8 @@ import 'package:onesthrm/page/authentication/bloc/authentication_bloc.dart';
 import 'package:onesthrm/page/menu_drawer/content/privacy_policy_content.dart';
 import 'package:onesthrm/res/nav_utail.dart';
 
+import '../../language/bloc/language_bloc.dart';
+
 class SupportContent extends StatelessWidget {
   const SupportContent({
     super.key,
@@ -13,94 +15,98 @@ class SupportContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(
-          height: 10,
-        ),
-        const Text(
-          'Support',
-          style: TextStyle(color: Colors.grey),
-        ),
-        const Divider(),
-        ListTile(
-          onTap: () {
-            NavUtil.navigateScreen(
-                context,
-                const PolicyContentScreen(
-                  appBarName: "support_policy",
-                  apiSlug: "support-24-7",
-                ));
-          },
-          dense: true,
-          contentPadding: EdgeInsets.zero,
-          horizontalTitleGap: 8,
-          leading:
-              SvgPicture.asset("assets/menu_drawer_icons/support-policy.svg"),
-          title: const Text('support_policy').tr(),
-        ),
-        ListTile(
-          onTap: () {
-            NavUtil.navigateScreen(
-                context,
-                const PolicyContentScreen(
-                  appBarName: "privacy_policy",
-                  apiSlug: "privacy-policy",
-                ));
-          },
-          dense: true,
-          contentPadding: EdgeInsets.zero,
-          horizontalTitleGap: 8,
-          leading:
-              SvgPicture.asset("assets/menu_drawer_icons/privacy-policy.svg"),
-          title: const Text('privacy_policy').tr(),
-        ),
-        ListTile(
-          onTap: () {
-            NavUtil.navigateScreen(
-                context,
-                const PolicyContentScreen(
-                  appBarName: "terms_conditions",
-                  apiSlug: "terms-of-use",
-                ));
-          },
-          dense: true,
-          contentPadding: EdgeInsets.zero,
-          horizontalTitleGap: 8,
-          leading:
-              SvgPicture.asset("assets/menu_drawer_icons/terms-condition.svg"),
-          title: const Text('terms_conditions').tr(),
-        ),
-        BlocBuilder<AuthenticationBloc, AuthenticationState>(
-            builder: (context, state) {
-          return ListTile(
-            onTap: () => showDialog(
-              context: context,
-              builder: (ctx) => AlertDialog(
-                content: const Text('Are you sure, you want to Logout?'),
-                actions: [
-                  TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: const Text('No')),
-                  TextButton(
-                      onPressed: () {
-                        BlocProvider.of<AuthenticationBloc>(context)
-                            .add(AuthenticationLogoutRequest());
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text('Yes')),
-                ],
-              ),
+    return BlocBuilder<LanguageBloc, LanguageState>(
+      builder: (context,state){
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 10,
             ),
-            dense: true,
-            contentPadding: EdgeInsets.zero,
-            horizontalTitleGap: 8,
-            leading: SvgPicture.asset("assets/menu_drawer_icons/logout.svg"),
-            title: const Text('Logout').tr(),
-          );
-        })
-      ],
+            const Text(
+              'Support',
+              style: TextStyle(color: Colors.grey),
+            ),
+            const Divider(),
+            ListTile(
+              onTap: () {
+                NavUtil.navigateScreen(
+                    context,
+                    const PolicyContentScreen(
+                      appBarName: "support_policy",
+                      apiSlug: "support-24-7",
+                    ));
+              },
+              dense: true,
+              contentPadding: EdgeInsets.zero,
+              horizontalTitleGap: 8,
+              leading:
+              SvgPicture.asset("assets/menu_drawer_icons/support-policy.svg"),
+              title: const Text('support_policy').tr(),
+            ),
+            ListTile(
+              onTap: () {
+                NavUtil.navigateScreen(
+                    context,
+                    const PolicyContentScreen(
+                      appBarName: "privacy_policy",
+                      apiSlug: "privacy-policy",
+                    ));
+              },
+              dense: true,
+              contentPadding: EdgeInsets.zero,
+              horizontalTitleGap: 8,
+              leading:
+              SvgPicture.asset("assets/menu_drawer_icons/privacy-policy.svg"),
+              title: const Text('privacy_policy').tr(),
+            ),
+            ListTile(
+              onTap: () {
+                NavUtil.navigateScreen(
+                    context,
+                    const PolicyContentScreen(
+                      appBarName: "terms_conditions",
+                      apiSlug: "terms-of-use",
+                    ));
+              },
+              dense: true,
+              contentPadding: EdgeInsets.zero,
+              horizontalTitleGap: 8,
+              leading:
+              SvgPicture.asset("assets/menu_drawer_icons/terms-condition.svg"),
+              title: const Text('terms_conditions').tr(),
+            ),
+            BlocBuilder<AuthenticationBloc, AuthenticationState>(
+                builder: (context, state) {
+                  return ListTile(
+                    onTap: () => showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                        content: const Text('Are you sure, you want to Logout?'),
+                        actions: [
+                          TextButton(
+                              onPressed: () => Navigator.of(context).pop(),
+                              child: const Text('No')),
+                          TextButton(
+                              onPressed: () {
+                                BlocProvider.of<AuthenticationBloc>(context)
+                                    .add(AuthenticationLogoutRequest());
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Yes')),
+                        ],
+                      ),
+                    ),
+                    dense: true,
+                    contentPadding: EdgeInsets.zero,
+                    horizontalTitleGap: 8,
+                    leading: SvgPicture.asset("assets/menu_drawer_icons/logout.svg"),
+                    title: const Text('Logout').tr(),
+                  );
+                })
+          ],
+        );
+      },
     );
   }
 }
