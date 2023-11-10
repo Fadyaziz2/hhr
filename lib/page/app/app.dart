a117f4b78f3523d64eff04eaa37f51157eedbe61
Saiful <onestdev105@gmail.com>
2023-11-10 17:06:43 +0600

    work on dashboard resize

--- lib/page/app/app.dart
+++ lib/page/app/app.dart
@@ -65,7 +65,6 @@ class _AppViewState extends State<AppView> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
