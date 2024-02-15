36e3a00b31f27ccee795f106e014be6cd75fdb5d
impexdhaka <mh72156@gmail.com>
2024-02-15 15:52:43 +0600

    automatic attendance when selfie confirmed

--- packages/selfie_attendance/lib/src/view/attendance_selfie_screen.dart
+++ packages/selfie_attendance/lib/src/view/attendance_selfie_screen.dart
@@ -61,6 +61,7 @@ class _AttendanceSelfieScreenState extends State<AttendanceSelfieScreen> {
             child: Stack(children: [
               (controller.value.isInitialized) ? CameraPreview(controller) : Container(color: Colors.black, child: const Center(child: CircularProgressIndicator())),
               Positioned(left: 0.0,right: 0.0,top: 0.0,bottom: 0.0,child: Image.asset('assets/images/face.png',color: Colors.grey,)),
+              Positioned(top: 45.0,left: 25.0,child: IconButton.outlined(onPressed: ()=> Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_new_sharp,),color: Colors.black)),
             ]),
           ),
           Container(
