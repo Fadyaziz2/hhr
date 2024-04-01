4cdd595ba0fd336e257ed5a9713f2fb6cc027b4b
Faysal chowdhury <faysalsookh@gmail.com>
2024-04-01 10:01:11 +0600

    wokring on selfie attendance

--- packages/selfie_attendance/lib/src/view/content/selfie_preview_screen.dart
+++ packages/selfie_attendance/lib/src/view/content/selfie_preview_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
@@ -20,22 +21,24 @@ class SelfiePreviewScreen extends StatelessWidget {
         child: SafeArea(
           child: Stack(
             children: [
-              Container(
-                padding: const EdgeInsets.all(5),
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5))),
-                child: Column(mainAxisSize: MainAxisSize.max, children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * .8,
-                    width: double.infinity,
-                    child: Transform(alignment: Alignment.center, transform: Matrix4.rotationY(mirror),
-                        child: Image.file(File(picture.path), fit: BoxFit.cover)),
-                  ),
-                  const SizedBox(height: 16),
-                  callBackButton
-                ]),
-              ),
-              Positioned(top: 40.0,left: 25.0,child: IconButton.outlined(onPressed: ()=> Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_new_sharp,),color: Colors.black)),
+              Column(mainAxisSize: MainAxisSize.max, children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 1,
+                  width: double.infinity,
+                  child: Transform(alignment: Alignment.center, transform: Matrix4.rotationY(mirror),
+                      child: Image.file(File(picture.path), fit: BoxFit.cover)),
+                ),
+                // const SizedBox(height: 16),
+                // callBackButton
+              ]),
+              Positioned(top: 30.0,left: 25.0,child: IconButton.outlined(onPressed: ()=> Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_new_sharp,),color: Colors.black)),
+
+              // callBackButton
+              Positioned(
+                  bottom: 60,
+                  left: 25,
+                  right: 25,
+                  child: callBackButton)
             ],
           ),
         ),
