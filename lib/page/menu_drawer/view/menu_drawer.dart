685341d87e06ec0ea762aa5a96741869f36a646e
impexdhaka <mh72156@gmail.com>
2024-04-30 15:37:16 +0600

    App version and name impl in drawer

--- lib/page/menu_drawer/view/menu_drawer.dart
+++ lib/page/menu_drawer/view/menu_drawer.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:onesthrm/page/authentication/bloc/authentication_bloc.dart';
 import 'package:onesthrm/page/menu/bloc/menu_bloc.dart';
 import 'package:onesthrm/page/menu_drawer/content/menu_settings_content.dart';
@@ -72,7 +73,7 @@ class MenuDrawer extends StatelessWidget {
                       '${menuState.appName} : V${menuState.appVersion}',
                       maxLines: 1,
                       overflow: TextOverflow.ellipsis,
-                      style:  TextStyle(fontSize: 14.r),
+                      style:  GoogleFonts.adamina(fontSize: 14.r,color: Colors.grey,),
                     ),
                     const SizedBox(
                       height: 32.0,
