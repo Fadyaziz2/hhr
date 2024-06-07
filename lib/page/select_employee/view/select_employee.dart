692b81aca26da5653a72638d7f9fb59ad2ee4de7
impexdhaka <mh72156@gmail.com>
2024-06-07 11:57:33 +0600

    refactor code

--- lib/page/select_employee/view/select_employee.dart
+++ lib/page/select_employee/view/select_employee.dart
@@ -4,9 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:meta_club_api/meta_club_api.dart';
 import 'package:onesthrm/page/select_employee/content/get_employee_content.dart';
-import 'package:onesthrm/page/authentication/bloc/authentication_bloc.dart';
 import 'package:onesthrm/page/phonebook/bloc/phonebook_bloc.dart';
-import 'package:core/core.dart';
 import 'package:onesthrm/res/widgets/device_util.dart';
 
 class SelectEmployeePage extends StatelessWidget {
@@ -14,9 +12,6 @@ class SelectEmployeePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = context.read<AuthenticationBloc>().state.data;
-    final baseUrl = globalState.get(companyUrl);
-
     return BlocProvider(
       create: (_) => PhoneBookBloc(
           metaClubApiClient: MetaClubApiClient(
