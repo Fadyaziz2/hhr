c9a48709bb81e36022d5212aa940bd90cb4470cb
impexdhaka <mh72156@gmail.com>
2024-06-25 14:32:31 +0600

    code refactor

--- lib/page/onboarding/view/onboarding_page.dart
+++ lib/page/onboarding/view/onboarding_page.dart
@@ -16,7 +16,7 @@ typedef OnboardingPageFactory = OnboardingPage Function();
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
 
-  static Route route(){
+  static Route route() {
     final onBoarding = instance<OnboardingPageFactory>();
     return MaterialPageRoute(builder: (_) => onBoarding());
   }
@@ -29,78 +29,70 @@ class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.symmetric(vertical: 10),
-        decoration: BoxDecoration(
-            color: Colors.grey[100],
-            borderRadius: BorderRadius.circular(0)),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomButton(
-            title: "next".tr(),
-            padding: 16,
-            clickButton: () {
-              NavUtil.pushAndRemoveUntil(context, const LoginPage());
-            },
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(color: Colors.grey[100], borderRadius: BorderRadius.circular(0)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CustomButton(
+              title: "next".tr(),
+              padding: 16,
+              clickButton: () {
+                NavUtil.pushAndRemoveUntil(context, const LoginPage());
+              },
+            ),
           ),
         ),
-      ),
-      body: BlocBuilder<OnboardingBloc,OnboardingState>(
-        builder: (context,state){
-          if(state.companyListModel?.companyList?.length == 1 && !mounted){
-            NavUtil.pushAndRemoveUntil(context, const LoginPage());
-          }
-          return Stack(
-            children: [
-              Column(
-                children: [
-                  Image.asset(
-                    'assets/images/company_list.jpg',
-                    fit: BoxFit.cover, height: 260.h, width: double.infinity,
-                  ),
-                  const SizedBox(height: 16),
-                  Expanded(
-                    child: state.status == NetworkStatus.loading ? const GeneralListShimmer() : ListView.builder(
-                      itemCount: state.companyListModel?.companyList?.length ?? 0,
-                      itemBuilder: (BuildContext context, int index) {
-                            Company? company =  state.companyListModel?.companyList?[index];
-                        return Container(
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 16.0,
-                              vertical: 8.0),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                              BorderRadius.circular(12.0),
-                              border: Border.all(
-                                  color: colorPrimary)),
-                          child: RadioListTile<Company?>(
-                              title: Text(
-                                company?.companyName ?? '',
-                                style: Theme.of(context).textTheme.titleMedium,
-                              ),
-                              // subtitle: Text(
-                              //   company?.subdomain ?? '',
-                              //   style: Theme.of(context)
-                              //       .textTheme
-                              //       .labelSmall,
-                              // ),
-                              value: company,
-                              groupValue: state.selectedCompany,
-                              onChanged: (Company? value) {
-                                    context.read<OnboardingBloc>().add(OnSelectedCompanyEvent(selectedCompany: value!));
-                              }),
-                        );
-                      },
+        body: BlocBuilder<OnboardingBloc, OnboardingState>(
+          builder: (context, state) {
+            if (state.companyListModel?.companyList?.length == 1 && !mounted) {
+              NavUtil.pushAndRemoveUntil(context, const LoginPage());
+            }
+            return Stack(
+              children: [
+                Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/company_list.jpg',
+                      fit: BoxFit.cover,
+                      height: 260.h,
+                      width: double.infinity,
                     ),
-                  ),
-
-                ],
-              ),
-            ],
-          );
-        },
-      )
-    );
+                    const SizedBox(height: 16),
+                    Expanded(
+                      child: state.status == NetworkStatus.loading
+                          ? const GeneralListShimmer()
+                          : ListView.builder(
+                              itemCount: state.companyListModel?.companyList?.length ?? 0,
+                              itemBuilder: (BuildContext context, int index) {
+                                Company? company = state.companyListModel?.companyList?[index];
+                                return Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(12.0),
+                                      border: Border.all(color: colorPrimary)),
+                                  child: RadioListTile<Company?>(
+                                      title: Text(
+                                        company?.companyName ?? '',
+                                        style: Theme.of(context).textTheme.titleMedium,
+                                      ),
+                                      value: company,
+                                      groupValue: state.selectedCompany,
+                                      onChanged: (Company? value) {
+                                        context
+                                            .read<OnboardingBloc>()
+                                            .add(OnSelectedCompanyEvent(selectedCompany: value!));
+                                      }),
+                                );
+                              },
+                            ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        ));
   }
 }
